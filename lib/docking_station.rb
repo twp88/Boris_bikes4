2786cbc19d29241a8314c04002ea7d855205102d
Tom Page <tom.w.page@gmail.com>
2016-09-28 16:47:12 +0100

    All the exercises until ex12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,14 @@
 class DockingStation
   attr_reader :bike
-  #def initialize
-  @bike
-  #end
+
+
+
   def release_bike
-    Bike.new
-    #Should be one of the existing bikes, not a random new one
+    fail "No more bikes you fool!" unless defined? @bike
+    @bike
   end
+
+
   def dock(bike)
     @bike = bike
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,19 +3,38 @@ require './lib/bike'
 
 describe 'docking_station' do
   station = DockingStation.new
-  bike = Bike.new
 
-  it 'respond to release bike' do
-    expect(station).to respond_to(:release_bike)
+  describe 'Empty Docking Station' do
+
+    it 'respond to release bike' do
+      expect(station).to respond_to(:release_bike)
+    end
+
+    it 'docks a bike' do
+      expect(station).to respond_to(:dock).with(1).argument
+    end
+
+    it "doesn't release new bikes" do
+      expect{station.release_bike}.to raise_error('No more bikes you fool!')
+
+
+    end
   end
-  it 'gives bike' do
-    expect(station.release_bike).to be_kind_of(Bike)
-  end
-  it 'docks a bike' do
-    expect(station).to respond_to(:dock).with(1).argument
-  end
-  it 'showed docked bikes' do
-    station.dock(bike)
-    expect(station.bike).to be(bike)
+
+
+  describe 'Working bike docking station functions' do
+      bike = Bike.new
+      station.dock(bike)
+
+
+    it 'gives bike' do
+      expect(station.release_bike).to be_kind_of(Bike)
+    end
+
+    it 'showed docked bikes' do
+      expect(station.bike).to be(bike)
+    end
+
+
   end
 end
