f9fcd9c6c88bf92f38e59fa6a67671a2dd2d2e7f
Dagmara <daga.sz@hotmail.com>
2016-09-28 17:49:12 +0100

    Finished the damned rspec

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,10 @@
 class DockingStation
   attr_reader :bike
 
-
-
   def release_bike
-    fail "No more bikes you fool!" unless defined? @bike
+    fail "No more bikes you fool!" unless @bike
     @bike
   end
-
 
   def dock(bike)
     @bike = bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,9 @@ require './lib/docking_station'
 require './lib/bike'
 
 describe 'docking_station' do
-  station = DockingStation.new
+  let(:station) {DockingStation.new} #optional, but is apparently better practice for instantiation
 
-  describe 'Empty Docking Station' do
-
+  context 'Empty Docking Station' do #context - the same thing as describe, just a convention, see link
     it 'respond to release bike' do
       expect(station).to respond_to(:release_bike)
     end
@@ -14,27 +13,23 @@ describe 'docking_station' do
       expect(station).to respond_to(:dock).with(1).argument
     end
 
-    it "doesn't release new bikes" do
-      expect{station.release_bike}.to raise_error('No more bikes you fool!')
-
-
+    it "doesn't release new bikes when no bikes" do
+      expect{station.release_bike}.to raise_error("No more bikes you fool!")
     end
   end
 
-
-  describe 'Working bike docking station functions' do
-      bike = Bike.new
-      station.dock(bike)
-
+  context 'Working bike docking station functions' do
+    let(:bike) {Bike.new}
 
     it 'gives bike' do
+      station.dock(bike) #turned out these were "bleeding" into the other test, so repeating is the only way I found to make things work :P
       expect(station.release_bike).to be_kind_of(Bike)
     end
 
     it 'showed docked bikes' do
+      station.dock(bike)
       expect(station.bike).to be(bike)
     end
-
-
   end
+
 end
