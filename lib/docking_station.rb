87c44583a3592a35eaa7840545d154109c9405c6
Dagmara <daga.sz@hotmail.com>
2016-09-28 15:16:39 +0100

    Docking bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,13 @@
 class DockingStation
+  attr_reader :bike
+  #def initialize
+  @bike
+  #end
   def release_bike
+    Bike.new
+    #Should be one of the existing bikes, not a random new one
+  end
+  def dock(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,21 @@
 require './lib/docking_station'
+require './lib/bike'
+
 describe 'docking_station' do
-  f = DockingStation.new
+  station = DockingStation.new
+  bike = Bike.new
+
   it 'respond to release bike' do
-    expect(f).to respond_to(:release_bike)
+    expect(station).to respond_to(:release_bike)
+  end
+  it 'gives bike' do
+    expect(station.release_bike).to be_kind_of(Bike)
+  end
+  it 'docks a bike' do
+    expect(station).to respond_to(:dock).with(1).argument
+  end
+  it 'showed docked bikes' do
+    station.dock(bike)
+    expect(station.bike).to be(bike)
   end
 end
